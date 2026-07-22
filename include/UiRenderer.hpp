dcdba7b5f2161997cee17d1380e0ce882f4acaf7
Grzegorz <grzegorz2047@gmail.com>
2026-07-22 22:23:41 +0200

    ui: add explicit Citro2D renderer

---
+++ include/UiRenderer.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <citro2d.h>
+#include <citro3d.h>
+
+#include "UiState.hpp"
+
+class UiRenderer {
+public:
+    UiRenderer() = default;
+    ~UiRenderer();
+
+    UiRenderer(const UiRenderer&) = delete;
+    UiRenderer& operator=(const UiRenderer&) = delete;
+
+    [[nodiscard]] bool initialize();
+    void shutdown();
+
+    void renderStandalone(const UiState& state);
+    void renderBottom(const UiState& state);
+    void renderTopOverlay(C3D_RenderTarget* target, const UiState& state);
+
+private:
+    void drawCampaign(const UiState& state);
+    void drawMission(const UiState& state);
+    void drawDiagnostics(const UiState& state);
+    void drawText(const char* text, float x, float y, float scale, u32 color);
+    void drawButton(float x, float y, float width, float height, const char* label, bool selected);
+
+    C3D_RenderTarget* bottomTarget_ = nullptr;
+    C2D_TextBuf textBuffer_ = nullptr;
+};
