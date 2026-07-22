3501b24c73a73f0f360cb6865c5d6f17945695e9
Grzegorz <grzegorz2047@gmail.com>
2026-07-22 02:44:07 +0200

    ux: add tutorial onboarding state machine

---
+++ include/TutorialFlow.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstddef>
+
+enum class TutorialPhase {
+    BuildFirstTower,
+    ReadyToStart,
+    WaveRunning,
+    Victory,
+    Defeat,
+};
+
+class TutorialFlow {
+public:
+    void reset();
+    void update(std::size_t towerCount, bool waveCompleted, bool waveLost);
+    [[nodiscard]] bool requestWaveStart(std::size_t towerCount);
+
+    [[nodiscard]] TutorialPhase phase() const;
+    [[nodiscard]] bool waveRunning() const;
+    [[nodiscard]] bool finished() const;
+
+private:
+    TutorialPhase phase_ = TutorialPhase::BuildFirstTower;
+};
