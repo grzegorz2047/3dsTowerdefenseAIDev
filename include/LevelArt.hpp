4fb26ad8be1b1f7059c9fc0f7a246842b1e48074
Grzegorz <grzegorz2047@gmail.com>
2026-07-23 10:28:46 +0200

    art: expose deterministic geometry budgets

--- include/LevelArt.hpp
+++ include/LevelArt.hpp
@@ -4,6 +4,7 @@
 #include <cstdint>
 
 constexpr std::size_t kMaximumSceneProps = 48U;
+constexpr std::size_t kVerticesPerBox = 30U;
 
 enum class LevelTheme : std::uint8_t {
     Default,
@@ -41,4 +42,18 @@ struct SceneProp {
         case ScenePropType::Watchtower: return "Watchtower";
     }
     return "Rock";
+}
+
+[[nodiscard]] constexpr std::size_t scenePropVertexBudget(ScenePropType type) {
+    switch (type) {
+        case ScenePropType::Pine: return 3U * kVerticesPerBox;
+        case ScenePropType::Rock: return kVerticesPerBox;
+        case ScenePropType::Ruin: return 2U * kVerticesPerBox;
+        case ScenePropType::Banner: return 2U * kVerticesPerBox;
+        case ScenePropType::Barricade: return 3U * kVerticesPerBox;
+        case ScenePropType::Wagon: return 3U * kVerticesPerBox;
+        case ScenePropType::Cliff: return kVerticesPerBox;
+        case ScenePropType::Watchtower: return 2U * kVerticesPerBox;
+    }
+    return kVerticesPerBox;
 }
