a4a5124553bde6d77f6c6a643553464462d48c1a
Grzegorz <grzegorz2047@gmail.com>
2026-07-23 09:15:53 +0200

    input: expose detected extended controls

--- include/Input.hpp
+++ include/Input.hpp
@@ -2,11 +2,15 @@
 
 #include <3ds.h>
 
+#include "ExtendedControls.hpp"
+
 struct InputSnapshot {
     u32 down = 0;
     u32 held = 0;
     circlePosition circle{};
     touchPosition touch{};
+    ExtendedRawInput extendedRaw{};
+    ExtendedControlScheme extendedScheme = ExtendedControlScheme::Camera;
 
     [[nodiscard]] bool pressed(u32 key) const {
         return (down & key) != 0U;
@@ -19,9 +23,28 @@ struct InputSnapshot {
     [[nodiscard]] bool touching() const {
         return isHeld(KEY_TOUCH);
     }
+
+    [[nodiscard]] bool extendedAvailable() const {
+        return extendedRaw.available;
+    }
+
+    [[nodiscard]] ExtendedMappedInput extended() const {
+        return ExtendedControls::map(extendedRaw, extendedScheme);
+    }
 };
 
 class InputSystem {
 public:
-    [[nodiscard]] InputSnapshot poll() const;
+    InputSystem();
+
+    [[nodiscard]] InputSnapshot poll();
+    [[nodiscard]] bool extendedAvailable() const;
+    [[nodiscard]] ExtendedControlScheme extendedScheme() const;
+
+private:
+    bool extendedAvailable_ = false;
+    ExtendedControlScheme extendedScheme_ = ExtendedControlScheme::Camera;
 };
+
+[[nodiscard]] bool new3dsExtendedControlsAvailable();
+[[nodiscard]] ExtendedControlScheme activeExtendedControlScheme();
