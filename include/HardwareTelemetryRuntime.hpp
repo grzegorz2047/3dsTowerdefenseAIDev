fd46d7a3a8708264dbb9334942c152fedae8cdc1
Grzegorz <grzegorz2047@gmail.com>
2026-07-25 15:48:39 +0200

    telemetry: expose automatic runtime snapshot

--- include/HardwareTelemetryRuntime.hpp
+++ include/HardwareTelemetryRuntime.hpp
@@ -1,10 +1,6 @@
 #pragma once
 
-#include <cstddef>
-
 #include "HardwareTelemetry.hpp"
 
-void hardwareTelemetryInitializeDeviceProfile();
-void hardwareTelemetryBeginFrame(float gameCpuMilliseconds);
-void hardwareTelemetryFinishFrame(std::size_t freeLinearMemoryBytes, std::uint8_t eyeCount);
+void hardwareTelemetryRecordGameCpu(float milliseconds);
 const HardwareTelemetrySnapshot& hardwareTelemetrySnapshot();
